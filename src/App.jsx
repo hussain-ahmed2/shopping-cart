8c920a218c2079d2aea825a11a7443c160124f97
Hussain Ahmed <hussainahmed.vu@gmail.com>
2024-10-23 19:56:12 +0600

    wishlist btn and funtionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from "react";
 import Product from "./components/Product";
+import { TiShoppingCart } from "react-icons/ti";
+import { FaListCheck } from "react-icons/fa6";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [isCartActive, setIsCartActive] = useState(false);
+  const [isWishListActive, setIsWishListActive] = useState(false);
   const [cart, setCart] = useState([]);
+  const [wishList, setWishList] = useState([]);
 
   function handleCartClick() {
     setIsCartActive((prev) => !prev);
+  }
+  function handleWishListClick() {
+    setIsWishListActive((prev) => !prev);
   }
 
   const URL = "https://dummyjson.com/products";
@@ -20,6 +27,9 @@ function App() {
   function handleClearCart() {
     setCart([]);
   }
+  function handleClearWishList() {
+    setWishList([]);
+  }
 
   useEffect(() => {
     fetchProduct(URL);
@@ -29,13 +39,77 @@ function App() {
     <>
       <nav className="flex justify-between items-center px-10 h-14 border-b shadow fixed w-full bg-white">
         <h1 className="text-2xl font-bold">Shopping Cart</h1>
-        <button
-          onClick={handleCartClick}
-          className="font-semibold text-xl py-2 px-4 rounded-md text-white bg-yellow-500"
-        >
-          Cart
-        </button>
+        <div className="flex items-center gap-6">
+          <button onClick={handleWishListClick} className="flex items-center text-xl py-1 px-2 gap-1 rounded shadow border hover:shadow-red-500 hover:text-red-500 transition-all duration-150">
+            <FaListCheck />
+            <span className="text-base font-mono uppercase">WishList</span>
+          </button>
+          <button
+            onClick={handleCartClick}
+            className="flex items-center text-xl py-1 px-2 rounded shadow border hover:shadow-yellow-500 hover:text-yellow-500 transition-all duration-150"
+          >
+            <TiShoppingCart />
+            <span className="text-base font-mono">CART</span>
+          </button>
+        </div>
       </nav>
+
+      <div
+        className={`fixed flex flex-col top-20 min-w-96 right-10 p-3 bg-white border transition-all duration-150 shadow-lg ${
+          !isWishListActive && "hidden"
+        }`}
+      >
+        <h1 className="text-lg pb-3 px-5 flex justify-between ">
+          WishList{" "}
+          <button
+            onClick={handleWishListClick}
+            className="text-xl hover:text-yellow-500"
+          >
+            &#10005;
+          </button>
+        </h1>
+        <table className="table text-center">
+          {wishList.length > 0 && (
+            <thead>
+              <tr>
+                <th className="px-5">Product</th>
+                <th className="px-5">Price</th>
+              </tr>
+            </thead>
+          )}
+
+          <tbody>
+            {!wishList.length ? (
+              <tr>
+                <td>WishList is empty</td>
+              </tr>
+            ) : (
+              wishList.map((item) => (
+                <tr key={item.id}>
+                  <td className="flex gap-5 items-center">
+                    <img
+                      src={item.thumbnail}
+                      alt="cart item"
+                      className="w-16 h-16"
+                    />
+                    {item.title}
+                  </td>
+                  <td>{item.price}</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+
+        {wishList.length > 0 && (
+          <button
+            onClick={handleClearWishList}
+            className="self-end mt-3 me-5 px-3 py-1 text-md rounded-md font-semibold capitalize bg-yellow-400 hover:text-white hover:bg-neutral-600"
+          >
+            clear wishList
+          </button>
+        )}
+      </div>
 
       <div
         className={`fixed flex flex-col top-20 min-w-96 right-10 p-3 bg-white border transition-all duration-150 shadow-lg ${
@@ -63,22 +137,26 @@ function App() {
           )}
 
           <tbody>
-            {!cart.length
-              ? "Cart is empty"
-              : cart.map((item) => (
-                  <tr key={item.id}>
-                    <td className="flex gap-5 items-center">
-                      <img
-                        src={item.thumbnail}
-                        alt="cart item"
-                        className="w-16 h-16"
-                      />
-                      {item.title}
-                    </td>
-                    <td>{item.quantity}</td>
-                    <td>{(item.price * item.quantity).toFixed(2)}</td>
-                  </tr>
-                ))}
+            {!cart.length ? (
+              <tr>
+                <td>Cart is empty</td>
+              </tr>
+            ) : (
+              cart.map((item) => (
+                <tr key={item.id}>
+                  <td className="flex gap-5 items-center">
+                    <img
+                      src={item.thumbnail}
+                      alt="cart item"
+                      className="w-16 h-16"
+                    />
+                    {item.title}
+                  </td>
+                  <td>{item.quantity}</td>
+                  <td>{(item.price * item.quantity).toFixed(2)}</td>
+                </tr>
+              ))
+            )}
             {cart.length > 0 && (
               <tr>
                 <td className="font-bold">Total</td>
@@ -96,10 +174,18 @@ function App() {
           </tbody>
         </table>
 
-        {cart.length > 0 && <button onClick={handleClearCart} className="self-end mt-3 me-5 px-3 py-1 text-md rounded-md font-semibold capitalize bg-yellow-400 hover:text-white hover:bg-neutral-600">clear cart</button>}
+        {cart.length > 0 && (
+          <button
+            onClick={handleClearCart}
+            className="self-end mt-3 me-5 px-3 py-1 text-md rounded-md font-semibold capitalize bg-yellow-400 hover:text-white hover:bg-neutral-600"
+          >
+            clear cart
+          </button>
+        )}
       </div>
 
-      <div className="container grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-fit mx-auto gap-2 pt-16">
+      <h1 className="pt-16 text-center text-3xl font-bold">Products</h1>
+      <div className="container grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-fit mx-auto gap-2 pt-3 px-4">
         {products.map((product) => (
           <Product
             key={product.id}
@@ -109,9 +195,13 @@ function App() {
             description={product.description}
             price={product.price}
             setCart={setCart}
+            wishList={wishList}
+            setWishList={setWishList}
           />
         ))}
       </div>
+
+      <footer className="text-center p-5">by hussain</footer>
     </>
   );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,33 +1,89 @@
-function Product({ id, title, thumbnail, description, price, setCart }) {
+import { useEffect, useState } from "react";
+import { FaHeart } from "react-icons/fa";
+
+function Product({
+  id,
+  title,
+  thumbnail,
+  description,
+  price,
+  setCart,
+  wishList,
+  setWishList,
+}) {
+  const [wished, setWished] = useState(false);
 
   function descriptionMinimizer(description) {
     if (description.length > 50) {
-      const newDescription = description.split('');
-      description = '';
-      for(let i=0; i<50; i++){
+      const newDescription = description.split("");
+      description = "";
+      for (let i = 0; i < 50; i++) {
         description += newDescription[i];
       }
-      description += '...';
+      description += "...";
     }
     return description;
   }
 
   function handleClick() {
-    const product = {id: id, title: title, price: price, quantity: 1, thumbnail: thumbnail}
+    const product = {
+      id: id,
+      title: title,
+      price: price,
+      quantity: 1,
+      thumbnail: thumbnail,
+    };
     let exists = false;
-    setCart(prev => {
-      const updatedCart = prev.map(item => {
+    setCart((prev) => {
+      const updatedCart = prev.map((item) => {
         if (item.id === id) {
           exists = true;
-          return { ...item, quantity: item.quantity + 1 }; 
+          return { ...item, quantity: item.quantity + 1 };
         }
         return item;
       });
-      
-      return exists ? updatedCart : [...updatedCart, product]; 
+
+      return exists ? updatedCart : [...updatedCart, product];
     });
-    
   }
+
+  function handleWishClick() {
+    if (!wished) {
+      const product = {
+        id: id,
+        title: title,
+        price: price,
+        thumbnail: thumbnail,
+      };
+      let exists = false;
+      setWishList((prev) => {
+        const updatedCart = prev.map((item) => {
+          if (item.id === id) {
+            exists = true;
+            return { ...item, quantity: item.quantity + 1 };
+          }
+          return item;
+        });
+
+        return exists ? updatedCart : [...updatedCart, product];
+      });
+    } else {
+      setWishList(prev => prev.filter(item => item.id != id));
+    }
+    setWished(prev => !prev);
+  }
+
+  useEffect(() => {
+    let found = false;
+    for (let item of wishList) {
+      if (item.id == id) {
+        found = true;
+        break;
+      }
+    }
+    if (found) setWished(true);
+    else setWished(false);
+  }, [wished, wishList]);
 
   return (
     <div className="flex flex-col border shadow basis-[240px] hover:border-yellow-500 transition-all duration-100 rounded-md">
@@ -37,7 +93,20 @@ function Product({ id, title, thumbnail, description, price, setCart }) {
         alt="Product image"
       />
       <div className="px-3 pb-3">
-        <h2 className="text-xl font-bold text-yellow-500 flex justify-between">${price} <span><button onClick={handleClick} className="font-semibold capitalize text-sm px-2 py-1 rounded bg-yellow-500 hover:bg-lime-500 text-white transition-all duration-100">add to cart</button></span></h2>
+        <h2 className="text-xl font-bold text-yellow-500 flex justify-between items-center">
+          ${price}{" "}
+          <span className="text-black cursor-pointer transition-all duration-100 ease-linear" onClick={handleWishClick}>
+            <FaHeart fill={wished ? "red" : "#333"} />
+          </span>{" "}
+          <span>
+            <button
+              onClick={handleClick}
+              className="font-semibold capitalize text-sm px-2 py-1 rounded bg-yellow-500 hover:bg-lime-500 text-white transition-all duration-100"
+            >
+              add to cart
+            </button>
+          </span>
+        </h2>
         <h3 className="text-lg font-semibold">{title}</h3>
         <p className="text-sm">{descriptionMinimizer(description)}</p>
       </div>
